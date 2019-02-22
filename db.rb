0282bdd8fb7d9ed3284c3311bd6e55aa8dc95f69
Andrew Pierce <apierce1992@gmail.com>
2019-02-22 13:28:53 -0500

    The web server now does a basic read of the local database we created and displays the users

---
+++ db.rb
@@ -0,0 +1,30 @@
+require 'pg'
+
+#This should only happen once! When we run fnz new and supply a project name we will run this and create the database with the project name
+#
+#conn = PG.connect(dbname: 'postgres')
+#conn.exec("CREATE DATABASE foo")
+
+conn = PG.connect(dbname: 'foo')
+
+create_query = <<-SQL
+CREATE TABLE users (
+userID int,
+first_name varchar(255),
+last_name varchar(255)
+);
+SQL
+
+insert_query = <<-SQL
+INSERT INTO users ( 
+first_name, last_name) 
+VALUES ( 
+'Andrew', 'Pierce');
+SQL
+
+#conn.exec(create_query)
+#conn.exec(insert_query)
+result = conn.exec("SELECT * FROM users")
+result.each do |row| 
+  puts row
+end
--- http.rb
+++ http.rb
@@ -1,16 +1,24 @@
 require 'socket'
+require 'pg'
 
 server = TCPServer.new 2000 # Server bound to port 2000
+
+def get_users
+  conn = PG.connect(dbname: 'foo')
+  conn.exec("SELECT * FROM users")
+end
 
 loop do
   client = server.accept    # Wait for a client to connect
   request = client.gets
-  puts request
-  puts client
+  users = get_users
 
   client.print "HTTP/1.1 200\r\n"
   client.print "Content-Type: text/html\r\n"
   client.print "\r\n"
-  client.print "Time is #{Time.now}"
+  client.puts "<p>The users in the table are:</p>"
+  users.each do |user|
+    client.puts "<p>#{user["first_name"]} #{user["last_name"]}</p>"
+  end
   client.close
 end
